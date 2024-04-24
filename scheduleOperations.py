24674a7494b5065ce681660e5cd518b06ea37749
seodowa <pagalan.theodoreustp@gmail.com>
2024-04-24 16:49:49 +0800

    updated scheduleOperations.py

--- scheduleOperations.py
+++ scheduleOperations.py
@@ -5,7 +5,10 @@ import os, time
 
 ADD_SCHEDS = []
 
-def insert_sched(day: str, subject: str, start_time: str, end_time: str, document) -> int:
+
+def insert_sched(
+    day: str, subject: str, start_time: str, end_time: str, document
+) -> int:
     table = document.tables[0]
     header_cells = table.rows[0].cells
 
@@ -16,62 +19,71 @@ def insert_sched(day: str, subject: str, start_time: str, end_time: str, documen
                 # check if the cell is empty
                 if table.columns[column].cells[row].text.strip() == "":
                     # put text in that cell and center it in the cell
-                    table.columns[column].cells[row].text = f"{subject}\n({start_time} - {end_time})"
+                    table.columns[column].cells[
+                        row
+                    ].text = f"{subject}\n({start_time} - {end_time})"
                     p = table.columns[column].cells[row].paragraphs[0]
                     p.alignment = WD_TABLE_ALIGNMENT.CENTER
                     return 0
-                
+
                 # if it's the last cell in the column and it is not blank, then create a new row and place the text there
-                if (row == len(table.columns[column].cells) - 1):
+                if row == len(table.columns[column].cells) - 1:
                     table.add_row().cells
-                    table.columns[column].cells[row + 1].text = f"{subject}\n({start_time} - {end_time})"
+                    table.columns[column].cells[
+                        row + 1
+                    ].text = f"{subject}\n({start_time} - {end_time})"
                     p = table.columns[column].cells[row + 1].paragraphs[0]
                     p.alignment = WD_TABLE_ALIGNMENT.CENTER
                     return 0
     return 1
 
+
 def sortSched(schedList: list) -> int:
     if len(schedList) == 0:
         return 1
-    
+
     schedList.sort(key=lambda x: x[2])
     for i in range(len(schedList)):
         schedList[i][2] = time.strftime("%I:%M %p", schedList[i][2])
 
     return 0
 
+
 def generate_sched(saveFileName: str, loadFileName: str) -> int:
     sortFailed = sortSched(ADD_SCHEDS)
     if sortFailed:
         return 4
-    
+
     try:
         # open the template file
         # make sure not to overwrite template.docx!!
         document = Document(f"{os.getcwd()}/Schedules/{loadFileName}")
-    except PackageNotFoundError:
-        print("Invalid document.")
-        return 1
-    
-    for day, subject, startTime, endTime in ADD_SCHEDS:
-        insertFailed = insert_sched(day, subject, startTime, endTime, document)
+
+        for day, subject, startTime, endTime in ADD_SCHEDS:
+            insertFailed = insert_sched(day, subject, startTime, endTime, document)
 
         # if failed to insert sched for sum reason
         if insertFailed:
             return 1
-    try:
+
         document.save(f"Schedules/{saveFileName}.docx")
+
+    except PackageNotFoundError:
+        print("Invalid document.")
+        return 1
+
     except PermissionError:
         for i in range(len(ADD_SCHEDS)):
             # revert the start time in string format back to struct_time format since sortSched() already converted the start time to string format
             # this is to avoid a type error in converting to and from struct_time format
-            ADD_SCHEDS[i][2] = time.strptime(ADD_SCHEDS[i][2], "%I:%M %p") 
+            ADD_SCHEDS[i][2] = time.strptime(ADD_SCHEDS[i][2], "%I:%M %p")
         return 2
-    
+
     # clear the ADD_SCHEDS array
     ADD_SCHEDS.clear()
 
     return 0
+
 
 if __name__ == "__main__":
     # ignore
